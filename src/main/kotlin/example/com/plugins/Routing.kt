faf2c9c24dfee3b396e71e52812c9ba5bd85d174
okaymisba <msbahsarfaraz@gmail.com>
2024-08-13 13:32:59 +0500

    first commit

---
+++ src/main/kotlin/example/com/plugins/Routing.kt
@@ -0,0 +1,21 @@
+package example.com.plugins
+
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.http.content.*
+import io.ktor.server.plugins.statuspages.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import java.io.File
+
+fun Application.configureRouting() {
+    install(StatusPages) {
+        exception<Throwable> { call, cause ->
+            call.respondText(text = "500: $cause" , status = HttpStatusCode.InternalServerError)
+        }
+    }
+    routing {
+         staticFiles("/", File("files"))
+        }
+    }
+
